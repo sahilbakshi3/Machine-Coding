102d293a945d3b6343f95e4faf9788e3f520e8ce
unknown <sahilbakshi3@example.com>
2025-11-26 15:38:01 +0530

    memeory game finished

---
+++ memory-game/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MemoryGame from "./components/MemoryGame";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <MemoryGame />
+    </div>
+  );
+};
+
+export default App;
