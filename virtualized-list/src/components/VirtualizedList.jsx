ab49a4e848810f44455f4c0196905393477c3503
unknown <sahilbakshi3@example.com>
2025-12-11 18:07:04 +0530

    virtualised list comp

--- virtualized-list/src/components/VirtualizedList.jsx
+++ virtualized-list/src/components/VirtualizedList.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VirtualizedList = ({ list, height, itemHeight, width }) => {
-  return <div>VirtualizedList</div>;
+  const [indices, setIndices] = useState([0, Math.floor(height / itemHeight)]);
+
+  const visibleList = list.slice(indices[0], indices[1] + 1);
+
+  const handleScroll = (e) => {
+    const { scrollTop } = e.target;
+
+    const newStartIndex = Math.floor(scrollTop / itemHeight);
+    const newEndIndex = newStartIndex + Math.floor(height / itemHeight);
+
+    setIndices([newStartIndex, newEndIndex]);
+  };
+
+  return (
+    <div
+      onScroll={handleScroll}
+      className="container"
+      style={{ height, width, backgroundColor: "grey", overflow: "auto" }}
+    >
+      <div style={{ height: list.length * itemHeight, position: "relative" }}>
+        {visibleList.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="item"
+              style={{
+                height: itemHeight,
+                background: "coral",
+                borderTop: "5px solid grey",
+                position: "absolute",
+                top: (indices[0] + index) * itemHeight,
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              {"Item" + item}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default VirtualizedList;
-
-// TODO - make the virtualised list /complete
