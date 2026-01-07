fdd62fb49b3c5ebc87f0e9f572afe5940a67caf2
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-01-07 14:07:29 +0530

    holy grail comp

---
+++ holy-grail/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <header>Header</header>
+      <div className="body">
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default App;
